17ba6ce351d6ceca46a85ce511398b6b3762ec42
Florian Burghardt <fl.burghardt@ext.douglas.eu>
2024-04-04 12:16:33 +0200

    Add UnitTests Project

---
+++ UnitTests/Controllers/ToDoControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using ToDoAPI.Controllers;
+using ToDoAPI.Models;
+using ToDoAPI.Repositories.Interfaces;
+
+namespace UnitTests.Controllers
+{
+    public class ToDoControllerTests
+    {
+        private ILogger<ToDoController> subLogger;
+        private IRepository<ToDo> subRepository;
+
+        public ToDoControllerTests()
+        {
+            this.subLogger = Substitute.For<ILogger<ToDoController>>();
+            this.subRepository = Substitute.For<IRepository<ToDo>>();
+        }
+
+        private ToDoController CreateToDoController()
+        {
+            return new ToDoController(
+                this.subLogger,
+                this.subRepository);
+        }
+
+        [Fact]
+        public void GetAll_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ UnitTests/Repositories/Implementation/RepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿namespace UnitTests.Repositories.Implementation
+{
+    public class RepositoryTests
+    {
+
+        public RepositoryTests()
+        {
+        }
+
+        [Fact]
+        public void Add_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Update_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Delete_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
